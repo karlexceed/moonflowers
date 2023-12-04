06a93a71383b29e241f0f19b8fa24e42c24d4bfc
karlexceed <alex.decker@gmail.com>
2023-12-03 21:23:25 -0600

    Adjust spawning
    Make frequency more rare, spawn placement more generic

--- mapgen.lua
+++ mapgen.lua
@@ -15,17 +15,17 @@ if mg_name == "v6" then
 		place_on = {"default:dirt_with_grass", "default:dirt_with_coniferous_litter", "default:dirt_with_snow", "default:dry_dirt_with_dry_grass", "default:dirt_with_rainforest_litter"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0,
+			offset = -0.01,
 			scale = 0.05,
 			spread = {x = 200, y = 200, z = 200},
-			seed = 436,
+			seed = 238900,
 			octaves = 3,
 			persist = 0.6
 		},
 		y_max = 30,
 		y_min = 20,
 		decoration = "moonflowers:moonflower",
-		spawn_by = {"default:tree", "default:pine_tree", "default:jungletree"},
+		spawn_by = {"group:tree"},
 		num_spawn_by = 1,
 	})
 
@@ -41,10 +41,10 @@ else
 		place_on = {"default:dirt_with_grass", "default:dirt_with_coniferous_litter", "default:dirt_with_snow", "default:dry_dirt_with_dry_grass", "default:dirt_with_rainforest_litter"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0,
+			offset = -0.01,
 			scale = 0.05,
 			spread = {x = 200, y = 200, z = 200},
-			seed = 436,
+			seed = 238900,
 			octaves = 3,
 			persist = 0.6
 		},
@@ -52,7 +52,7 @@ else
 		y_max = 30,
 		y_min = 20,
 		decoration = "moonflowers:moonflower",
-		spawn_by = {"default:tree", "default:aspen_tree", "default:acacia_tree", "default:pine_tree", "default:jungletree"},
+		spawn_by = {"group:tree"},
 		num_spawn_by = 1,
 	})
 
